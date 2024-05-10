d737c69f2d62a2d6365e59a3ef725883908b9d12
Sayf-allah Gabsi <sgabsi@paul-f5Br6s7.clusters.42paris.fr>
2024-05-10 13:07:04 +0200

    enfin fini

--- src/so_long.c
+++ src/so_long.c
@@ -6,14 +6,14 @@
 /*   By: sgabsi <sgabsi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 16:15:30 by sgabsi            #+#    #+#             */
-/*   Updated: 2024/05/07 11:56:02 by sgabsi           ###   ########.fr       */
+/*   Updated: 2024/05/10 13:06:25 by sgabsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <X11/X.h>
 
-void	usage(void)
+static void	usage(void)
 {
 	ft_printf("Usage : ./so_long [chemin relatif de la map]\n");
 	exit(1);
@@ -22,11 +22,13 @@ void	usage(void)
 int	main(int argc, char *argv[])
 {
 	t_data	data;
+	char	*str;
 
 	if (argc != 2)
 		usage();
 	ft_bzero(&data, sizeof(data));
-	if (ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])) == NULL)
+	str = argv[1] + (ft_strlen(argv[1]) - 4);
+	if (str && ft_strncmp(str, ".ber", 4) != 0)
 		exit_error(NOTBER);
 	check_valid_map(&data, argv[1]);
 	if (!init_minilib(&data))
